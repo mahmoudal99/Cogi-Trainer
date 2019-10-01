179de47906558b38a2aee9a0dd64acdb2e930041
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-10-01 12:51:38 +0100

    Changed Settings Page Design

--- app/src/main/java/onipractice/mahmoud/com/fitnessapp/Profile/EditProfileActivity.java
+++ app/src/main/java/onipractice/mahmoud/com/fitnessapp/Profile/EditProfileActivity.java
@@ -1,46 +1,25 @@
 package onipractice.mahmoud.com.fitnessapp.Profile;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.NumberPicker;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.codetroopers.betterpickers.numberpicker.NumberPickerBuilder;
-import com.codetroopers.betterpickers.numberpicker.NumberPickerDialogFragment;
-import com.codetroopers.betterpickers.recurrencepicker.RecurrencePickerDialogFragment;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import onipractice.mahmoud.com.fitnessapp.R;
-import onipractice.mahmoud.com.fitnessapp.SignInActivity;
 import onipractice.mahmoud.com.fitnessapp.Utils.Dialogs;
-import onipractice.mahmoud.com.fitnessapp.Utils.FirebaseMethods;
 
 public class EditProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -64,7 +43,7 @@ public class EditProfileActivity extends AppCompatActivity implements View.OnCli
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        setContentView(R.layout.activity_settings);
 
         context = EditProfileActivity.this;
         userType = getIntent().getStringExtra("user");
